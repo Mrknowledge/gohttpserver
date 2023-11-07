90effd91766cab143bfff0b1b6ee3f9817f66909
yifu.wang <yifu.wang@zenlayer.com>
2023-11-07 17:02:20 +0800

    feat: add modify config

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,47 +1,48 @@
-jQuery('#qrcodeCanvas').qrcode({
-  text: "http://jetienne.com/"
+jQuery("#qrcodeCanvas").qrcode({
+  text: "http://jetienne.com/",
 });
 
 Dropzone.autoDiscover = false;
 
 function getExtention(fname) {
-  return fname.slice((fname.lastIndexOf(".") - 1 >>> 0) + 2);
+  return fname.slice(((fname.lastIndexOf(".") - 1) >>> 0) + 2);
 }
 
 function pathJoin(parts, sep) {
-  var separator = sep || '/';
-  var replace = new RegExp(separator + '{1,}', 'g');
+  var separator = sep || "/";
+  var replace = new RegExp(separator + "{1,}", "g");
   return parts.join(separator).replace(replace, separator);
 }
 
 function getQueryString(name) {
   var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
   var r = decodeURI(window.location.search).substr(1).match(reg);
-  if (r != null) return r[2].replace(/\+/g, ' ');
+  if (r != null) return r[2].replace(/\+/g, " ");
   return null;
 }
 
 function checkPathNameLegal(name) {
   var reg = new RegExp("[\\/:*<>|]");
-  var r = name.match(reg)
+  var r = name.match(reg);
   return r == null;
 }
 
 function showErrorMessage(jqXHR) {
-  let errMsg = jqXHR.getResponseHeader("x-auth-authentication-message")
+  let errMsg = jqXHR.getResponseHeader("x-auth-authentication-message");
   if (errMsg == null) {
-    errMsg = jqXHR.responseText
+    errMsg = jqXHR.responseText;
   }
   alert(String(jqXHR.status).concat(":", errMsg));
-  console.error(errMsg)
+  console.error(errMsg);
 }
-
 
 var vm = new Vue({
   el: "#app",
   data: {
-    currentFileName:'',
-    editFileName:'',
+    userInfo: [],
+    authInfo: { users: [], accessTables: [] },
+    currentFileName: "",
+    editFileName: "",
     user: {
       email: "",
       name: "",
@@ -51,24 +52,26 @@ var vm = new Vue({
     showHidden: false,
     previewMode: false,
     preview: {
-      filename: '',
-      filetype: '',
+      filename: "",
+      filetype: "",
       filesize: 0,
-      contentHTML: '',
+      contentHTML: "",
     },
     version: "loading",
     mtimeTypeFromNow: false, // or fromNow
     auth: {},
     search: getQueryString("search"),
-    files: [{
-      name: "loading ...",
-      path: "",
-      size: "...",
-      type: "dir",
-    }],
+    files: [
+      {
+        name: "loading ...",
+        path: "",
+        size: "...",
+        type: "dir",
+      },
+    ],
     myDropzone: null,
     nameSort: true,
-    timeSort: true
+    timeSort: true,
   },
   computed: {
     computedFiles: function () {
@@ -76,10 +79,10 @@ var vm = new Vue({
       that.preview.filename = null;
 
       var files = this.files.filter(function (f) {
-        if (f.name == 'README.md') {
+        if (f.name == "README.md") {
           that.preview.filename = f.name;
         }
-        if (!that.showHidden && f.name.slice(0, 1) === '.') {
+        if (!that.showHidden && f.name.slice(0, 1) === ".") {
           return false;
         }
         return true;
@@ -87,10 +90,10 @@ var vm = new Vue({
       // console.log(this.previewFile)
       if (this.preview.filename) {
         var name = this.preview.filename; // For now only README.md
-        console.log(pathJoin([location.pathname, 'README.md']))
+        console.log(pathJoin([location.pathname, "README.md"]));
         $.ajax({
-          url: pathJoin([location.pathname, 'README.md']),
-          method: 'GET',
+          url: pathJoin([location.pathname, "README.md"]),
+          method: "GET",
           success: function (res) {
             var converter = new showdown.Converter({
               tables: true,
@@ -109,9 +112,9 @@ var vm = new Vue({
             that.preview.contentHTML = html;
           },
           error: function (err) {
-            console.log(err)
-          }
-        })
+            console.log(err);
+          },
+        });
       }
 
       return files;
@@ -127,8 +130,8 @@ var vm = new Vue({
           this.user.email = ret.email;
           this.user.name = ret.name;
         }
-      }.bind(this)
-    })
+      }.bind(this),
+    });
     this.myDropzone = new Dropzone("#upload-form", {
       paramName: "file",
       maxFilesize: 10240,
@@ -139,28 +142,27 @@ var vm = new Vue({
         });
         this.on("complete", function (file) {
           //console.log("reload file list")
-          loadFileList()
-        })
-      }
+          loadFileList();
+        });
+      },
     });
   },
   methods: {
-    nameSortFunc: function(){
+    nameSortFunc: function () {
       this.nameSort = !this.nameSort;
       if (this.nameSort) {
-        this.files = _.sortBy(this.files, 'name')
-      }else{
-        this.files = _.sortBy(this.files, 'name').reverse()
+        this.files = _.sortBy(this.files, "name");
+      } else {
+        this.files = _.sortBy(this.files, "name").reverse();
       }
-      console.log(this.files)
-      
+      console.log(this.files);
     },
-    timeSortFunc: function(){
+    timeSortFunc: function () {
       this.timeSort = !this.timeSort;
       if (this.timeSort) {
-        this.files = _.sortBy(this.files, 'mtime')
-      }else{
-        this.files = _.sortBy(this.files, 'mtime').reverse()
+        this.files = _.sortBy(this.files, "mtime");
+      } else {
+        this.files = _.sortBy(this.files, "mtime").reverse();
       }
     },
     getEncodePath: function (f) {
@@ -171,7 +173,7 @@ var vm = new Vue({
       if (this.mtimeTypeFromNow) {
         return m.fromNow();
       }
-      return m.format('YYYY-MM-DD HH:mm:ss');
+      return m.format("YYYY-MM-DD HH:mm:ss");
     },
     toggleHidden: function () {
       this.showHidden = !this.showHidden;
@@ -180,7 +182,7 @@ var vm = new Vue({
       this.myDropzone.removeAllFiles();
     },
     parentDirectory: function (path) {
-      return path.replace('\\', '/').split('/').slice(0, -1).join('/')
+      return path.replace("\\", "/").split("/").slice(0, -1).join("/");
     },
     changeParentDirectory: function (path) {
       var parentDir = this.parentDirectory(path);
@@ -203,25 +205,33 @@ var vm = new Vue({
       var urlPath = this.genInstallURL(name, true);
       $("#qrcode-title").html(title || name || location.pathname);
       $("#qrcode-link").attr("href", urlPath);
-      $('#qrcodeCanvas').empty().qrcode({
-        text: encodeURI(urlPath),
-      });
+      $("#qrcodeCanvas")
+        .empty()
+        .qrcode({
+          text: encodeURI(urlPath),
+        });
 
       $("#qrcodeRight a").attr("href", urlPath);
       $("#qrcode-modal").modal("show");
     },
     genDownloadURL: function (f) {
       var search = location.search;
-      var sep = search == "" ? "?" : "&"
-      return location.origin + this.getEncodePath(f) + location.search + sep + "download=true";
+      var sep = search == "" ? "?" : "&";
+      return (
+        location.origin +
+        this.getEncodePath(f) +
+        location.search +
+        sep +
+        "download=true"
+      );
     },
     shouldHaveQrcode: function (name) {
-      return ['apk', 'ipa'].indexOf(getExtention(name)) !== -1;
+      return ["apk", "ipa"].indexOf(getExtention(name)) !== -1;
     },
     genFileClass: function (f) {
       if (f.type == "dir") {
-        if (f.name == '.git') {
-          return 'fa-git-square';
+        if (f.name == ".git") {
+          return "fa-git-square";
         }
         return "fa-folder-open";
       }
@@ -256,7 +266,7 @@ var vm = new Vue({
         case "exe":
           return "fa-windows";
       }
-      return "fa-file-text-o"
+      return "fa-file-text-o";
     },
     clickFileOrDir: function (f, e) {
       var reqPath = pathJoin([location.pathname, encodeURIComponent(f.name)]);
@@ -266,11 +276,11 @@ var vm = new Vue({
         return;
       }
       loadFileOrDir(reqPath);
-      e.preventDefault()
+      e.preventDefault();
     },
     changePath: function (reqPath, e) {
       loadFileOrDir(reqPath);
-      e.preventDefault()
+      e.preventDefault();
     },
     showInfo: function (f) {
       console.log(f);
@@ -287,9 +297,9 @@ var vm = new Vue({
           // console.log(JSON.stringify(res, null, 4));
         },
         error: function (jqXHR, textStatus, errorThrown) {
-          showErrorMessage(jqXHR)
-        }
-      })
+          showErrorMessage(jqXHR);
+        },
+      });
     },
     showEditURL: function (f) {
       console.log(f);
@@ -297,66 +307,172 @@ var vm = new Vue({
       this.editFileName = f.name;
       $("#edit-modal").modal("show");
     },
-    genEditURL: function (){
+    viewConfig: function (type) {
+      const typeStr = type === "user" ? `&type=${type}` : "";
+      const that = this;
       $.ajax({
-        url: pathJoin([location.pathname, encodeURIComponent(this.currentFileName)]),
-        method: 'PUT',
+        url: pathJoin([location.pathname, "?op=conf" + typeStr]),
+        method: "GET",
+        success: function (res) {
+          const resJson = jsyaml.load(res);
+          if (type === "user") {
+            that.userInfo =
+              resJson?.users?.map((item) => {
+                const userArr = Object.entries(item);
+                return {
+                  name: userArr?.[0]?.[0],
+                  password: userArr?.[0]?.[1],
+                };
+              }) || [];
+            console.log(that.userInfo);
+            $("#edit-user-modal").modal("show");
+          } else {
+            console.log(resJson);
+            that.authInfo = resJson;
+            $("#edit-auth-modal").modal("show");
+          }
+        },
+      });
+    },
+    removeUserItem: function (idx) {
+      this.userInfo.splice(idx, 1);
+    },
+    addUserItem: function () {
+      this.userInfo.push({
+        name: undefined,
+        password: undefined,
+      });
+    },
+    editUserInfo: function () {
+      const resTemp = this.userInfo
+        ?.filter((item) => !!item?.name?.trim() && !!item?.password?.trim())
+        ?.map((item) => ({
+          [item.name]: item.password,
+        }));
+      this.setConfig("user", { users: resTemp });
+    },
+
+    removeAuthUserItem: function (idx) {
+      this.authInfo?.users?.splice(idx, 1);
+    },
+    addAuthUserItem: function () {
+      this.authInfo?.users?.push({
+        email: undefined,
+        token: undefined,
+        show: false,
+        upload: false,
+        delete: false,
+      });
+    },
+
+    removeAccessItem: function (idx) {
+      this.authInfo?.accessTables?.splice(idx, 1);
+    },
+    addAccessUserItem: function () {
+      this.authInfo?.accessTables?.push({
+        regex: undefined,
+        allow: false,
+      });
+    },
+
+    editAuthInfo: function () {
+      const resTemp = {
+        ...this.authInfo,
+        users: this.authInfo?.users?.filter(
+          (item) => !!item?.email?.trim() && !!item?.token?.trim()
+        ),
+        accessTables: this.authInfo?.accessTables?.filter(
+          (item) => !!item?.regex?.trim()
+        ),
+      };
+      this.setConfig("auth", resTemp);
+    },
+
+    setConfig: function (type, content) {
+      const typeStr = type === "user" ? `&type=${type}` : "";
+      const contentStr = `&content=${window.btoa(jsyaml.dump(content))}`;
+      $.ajax({
+        url: pathJoin([location.pathname, "?op=conf" + typeStr + contentStr]),
+        method: "PUT",
+        success: function () {
+          $("#edit-user-modal").modal("hide");
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+          showErrorMessage(jqXHR);
+        },
+      });
+    },
+    genEditURL: function () {
+      $.ajax({
+        url: pathJoin([
+          location.pathname,
+          encodeURIComponent(this.currentFileName),
+        ]),
+        method: "PUT",
         data: {
-          filename: this.editFileName
+          filename: this.editFileName,
         },
         success: function (res) {
-          loadFileList()
+          loadFileList();
           $("#edit-modal").modal("hide");
         },
         error: function (jqXHR, textStatus, errorThrown) {
-          showErrorMessage(jqXHR)
-        }
+          showErrorMessage(jqXHR);
+        },
       });
     },
     makeDirectory: function () {
-      var name = window.prompt("current path: " + location.pathname + "\nplease enter the new directory name", "")
-      console.log(name)
+      var name = window.prompt(
+        "current path: " +
+          location.pathname +
+          "\nplease enter the new directory name",
+        ""
+      );
+      console.log(name);
       if (!name) {
-        return
+        return;
       }
-      if(!checkPathNameLegal(name)) {
-        alert("Name should not contains any of \\/:*<>|")
-        return
+      if (!checkPathNameLegal(name)) {
+        alert("Name should not contains any of \\/:*<>|");
+        return;
       }
       $.ajax({
         url: pathJoin(["/", location.pathname, "/", encodeURIComponent(name)]),
         method: "POST",
         success: function (res) {
-          console.log(res)
-          loadFileList()
+          console.log(res);
+          loadFileList();
         },
         error: function (jqXHR, textStatus, errorThrown) {
-          showErrorMessage(jqXHR)
-        }
-      })
+          showErrorMessage(jqXHR);
+        },
+      });
     },
     deletePathConfirm: function (f, e) {
       e.preventDefault();
-      if (!e.altKey) { // skip confirm when alt pressed
-        if (!window.confirm("Delete " + location.pathname + "/" + f.name + " ?")) {
+      if (!e.altKey) {
+        // skip confirm when alt pressed
+        if (
+          !window.confirm("Delete " + location.pathname + "/" + f.name + " ?")
+        ) {
           return;
         }
       }
       $.ajax({
         url: pathJoin([location.pathname, encodeURIComponent(f.name)]),
-        method: 'DELETE',
+        method: "DELETE",
         success: function (res) {
-          loadFileList()
+          loadFileList();
         },
         error: function (jqXHR, textStatus, errorThrown) {
-          showErrorMessage(jqXHR)
-        }
+          showErrorMessage(jqXHR);
+        },
       });
     },
     updateBreadcrumb: function (pathname) {
       var pathname = decodeURI(pathname || location.pathname || "/");
-      pathname = pathname.split('?')[0]
-      var parts = pathname.split('/');
+      pathname = pathname.split("?")[0];
+      var parts = pathname.split("/");
       this.breadcrumb = [];
       if (pathname == "/") {
         return this.breadcrumb;
@@ -367,131 +483,130 @@ var vm = new Vue({
         if (!name) {
           continue;
         }
-        var path = parts.slice(0, i).join('/');
+        var path = parts.slice(0, i).join("/");
         this.breadcrumb.push({
-          name: name + (i == parts.length ? ' /' : ''),
-          path: path
-        })
+          name: name + (i == parts.length ? " /" : ""),
+          path: path,
+        });
       }
       return this.breadcrumb;
     },
     loadPreviewFile: function (filepath, e) {
       if (e) {
-        e.preventDefault() // may be need a switch
+        e.preventDefault(); // may be need a switch
       }
       var that = this;
-      $.getJSON(pathJoin(['/-/info', location.pathname]))
-          .then(function (res) {
-            console.log(res);
-            that.preview.filename = res.name;
-            that.preview.filesize = res.size;
-            return $.ajax({
-              url: '/' + res.path,
-              dataType: 'text',
-            });
-          })
-          .then(function (res) {
-            console.log(res)
-            that.preview.contentHTML = '<pre>' + res + '</pre>';
-            console.log("Finally")
-          })
-          .done(function (res) {
-            console.log("done", res)
+      $.getJSON(pathJoin(["/-/info", location.pathname]))
+        .then(function (res) {
+          console.log(res);
+          that.preview.filename = res.name;
+          that.preview.filesize = res.size;
+          return $.ajax({
+            url: "/" + res.path,
+            dataType: "text",
           });
+        })
+        .then(function (res) {
+          console.log(res);
+          that.preview.contentHTML = "<pre>" + res + "</pre>";
+          console.log("Finally");
+        })
+        .done(function (res) {
+          console.log("done", res);
+        });
     },
     loadAll: function () {
       // TODO: move loadFileList here
     },
-  }
-})
+  },
+});
 
 window.onpopstate = function (event) {
   if (location.search.match(/\?search=/)) {
     location.reload();
     return;
   }
-  loadFileList()
-}
+  loadFileList();
+};
 
 function loadFileOrDir(reqPath) {
-  let requestUri = reqPath + location.search
-  var retObj = loadFileList(requestUri)
+  let requestUri = reqPath + location.search;
+  var retObj = loadFileList(requestUri);
   if (retObj !== null) {
     retObj.done(function () {
       window.history.pushState({}, "", requestUri);
     });
   }
-
 }
 
 function loadFileList(pathname) {
   var pathname = pathname || location.pathname + location.search;
-  var retObj = null
-  if (getQueryString("raw") !== "false") { // not a file preview
-    var sep = pathname.indexOf("?") === -1 ? "?" : "&"
+  var retObj = null;
+  if (getQueryString("raw") !== "false") {
+    // not a file preview
+    var sep = pathname.indexOf("?") === -1 ? "?" : "&";
     retObj = $.ajax({
       url: pathname + sep + "json=true",
       dataType: "json",
       cache: false,
       success: function (res) {
         res.files = _.sortBy(res.files, function (f) {
-          var weight = f.type == 'dir' ? 1000 : 1;
+          var weight = f.type == "dir" ? 1000 : 1;
           return -weight * f.mtime;
-        })
+        });
         vm.files = res.files;
         vm.auth = res.auth;
         vm.updateBreadcrumb(pathname);
       },
       error: function (jqXHR, textStatus, errorThrown) {
-        showErrorMessage(jqXHR)
+        showErrorMessage(jqXHR);
       },
     });
-
   }
 
   vm.previewMode = getQueryString("raw") == "false";
   if (vm.previewMode) {
     vm.loadPreviewFile();
   }
-  return retObj
+  return retObj;
 }
 
-Vue.filter('fromNow', function (value) {
+Vue.filter("fromNow", function (value) {
   return moment(value).fromNow();
-})
+});
 
-Vue.filter('formatBytes', function (value) {
+Vue.filter("formatBytes", function (value) {
   var bytes = parseFloat(value);
   if (bytes < 0) return "-";
   else if (bytes < 1024) return bytes + " B";
   else if (bytes < 1048576) return (bytes / 1024).toFixed(0) + " KB";
   else if (bytes < 1073741824) return (bytes / 1048576).toFixed(1) + " MB";
   else return (bytes / 1073741824).toFixed(1) + " GB";
-})
+});
 
 $(function () {
   $.scrollUp({
-    scrollText: '', // text are defined in css
+    scrollText: "", // text are defined in css
   });
 
   // For page first loading
-  loadFileList(location.pathname + location.search)
+  loadFileList(location.pathname + location.search);
 
   // update version
   $.getJSON("/-/sysinfo", function (res) {
     vm.version = res.version;
-  })
+  });
 
-  var clipboard = new Clipboard('.btn');
-  clipboard.on('success', function (e) {
-    console.info('Action:', e.action);
-    console.info('Text:', e.text);
-    console.info('Trigger:', e.trigger);
+  var clipboard = new Clipboard(".btn");
+  clipboard.on("success", function (e) {
+    console.info("Action:", e.action);
+    console.info("Text:", e.text);
+    console.info("Trigger:", e.trigger);
     $(e.trigger)
-        .tooltip('show')
-        .mouseleave(function () {
-          $(this).tooltip('hide');
-        })
+      .tooltip("show")
+      .mouseleave(function () {
+        $(this).tooltip("hide");
+      });
 
     e.clearSelection();
   });
